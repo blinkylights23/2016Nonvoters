f39c3c35ac4393cff713a7d5cc3bc000ff17767c
Paul Smith <paul@blinkylights.org>
2016-11-19 04:50:20 -0500

    Map-based visualization

--- app/js/app.js
+++ app/js/app.js
@@ -5,35 +5,82 @@ import * as d3_selection from 'd3-selection';
 import 'd3-selection-multi';
 var d3 = Object.assign({}, d3Obj);
 
-console.log(d3);
+var mapWidth = 800,
+    mapHeight = 2.0 * (mapWidth / 3.0);
 
 
-d3.csv('/data/2016Results.csv', function(err, data) {
-  var ecvScale = d3.scaleLinear()
-    .domain([3, 55])
-    .range([10, 50]);
 
-  var svg = d3.select('#visualization svg');
-  svg.selectAll('circle')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attrs({
-      r: d => ecvScale(d.ecv)/2,
-      cx: (d, i) => 120 + ((i % 10) * 120),
-      cy: (d, i) => 100 + (Math.floor(i/10) * 100),
-      fill: (d, i) => { return parseInt(d.clinton) <= parseInt(d.trump) ? 'red' : 'blue'; }
+var projection = d3.geoAlbersUsa()
+  .translate([Math.floor(mapWidth/2), Math.floor(mapHeight/2)])
+  .scale([mapWidth * 1.3]);
+
+var path = d3.geoPath().projection(projection);
+
+var tooltipDiv = d3.select('body').append('div')
+  .attr('class', 'tooltip')
+  .style('opacity', 0);
+
+var voterData = d3.select('div#voterData');
+
+d3.json('/data/us-states.json', function(json) {
+  d3.csv('/data/2016Results.csv', function(err, data) {
+
+    data.forEach(d => {
+      json.features.forEach(j => {
+        if (d.state == j.properties.name) {
+          j.properties.data = d;
+        }
+      });
     });
-  svg.selectAll('text')
-    .data(data)
-    .enter()
-    .append('text')
-    .attrs({
-      x: (d, i) => 120 + ((i % 10) * 120),
-      y: (d, i) => 135 + (Math.floor(i/10) * 100),
-      'text-anchor': 'middle',
-      'font-family': 'sans-serif',
-      fill: 'black'
-    })
-    .text(d => `${d.abbr}(${d.ecv})`);
+
+    var mapSvg = d3.select('#visualization svg#map')
+      .attr('width', mapWidth)
+      .attr('height', mapHeight)
+      .style('border', '1px solid #aaa');
+    var g = mapSvg.append('g');
+
+    g.append('g').attr('class', 'statesMap')
+      .selectAll('path')
+	    .data(json.features.filter(f => f.properties.data))
+	    .enter()
+	    .append('path')
+	    .attr('d', path)
+	    .style('stroke', '#fff')
+	    .style('stroke-width', '1')
+	    .style('fill', (d, i) => {
+        return parseInt(d.properties.data.clinton) <= parseInt(d.properties.data.trump) ? '#900' : '#009';
+      })
+      .on('mouseover', stateMouseover)
+      .on('mouseout', stateMouseout)
+      .on('click', stateClick);
+
+  });
 });
+
+
+
+function stateMouseover(d, i) {
+  console.log(d3Obj.event.pageX, d3Obj.event.pageY);
+  tooltipDiv.transition()
+    .duration(200)
+    .style('opacity', '1.0');
+  tooltipDiv.html(`${d.properties.data.abbr} [${d.properties.data.ecv}]`)
+    .style('left', d3Obj.event.pageX + 'px')
+    .style('top', d3Obj.event.pageY + 'px');
+}
+
+function stateMouseout(d, i) {
+  // console.log('mouseout', d, i);
+  tooltipDiv.transition()
+    .duration(500)
+    .style('opacity', 0);
+}
+
+function stateClick(d, i) {
+  console.log('click', d, i);
+}
+
+
+
+
+
